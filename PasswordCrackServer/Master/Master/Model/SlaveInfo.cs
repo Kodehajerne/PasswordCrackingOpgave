8ca7f3981dfcd07d023fef63a9edcecc6bad2ccd
wandriey <jona434g@edu.easj.dk>
2018-10-16 20:08:32 +0200

    SlaveInfo class add
    The class contrains all the specific attributes belonging to a slave: Ip and port

---
+++ PasswordCrackServer/Master/Master/Model/SlaveInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Master.Model
+{
+    public class SlaveInfo
+    {
+        private string _ip;
+            private int _port;
+
+        public SlaveInfo(string ip, int port)
+        {
+            _ip = ip;
+            _port = port;
+        }
+
+        public string getIp
+        {
+            get { return _ip; }
+            set { _ip = value; }
+        }
+        public int getPort
+        {
+            get { return _port; }
+            set { _port = value; }
+        }
+    }
+}
